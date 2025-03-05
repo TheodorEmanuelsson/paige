91794a06a4cbd86a170a74ca4787fc38cda88c93
TheodorEmanuelsson <theodor.emanuelsson@einride.tech>
2025-03-05 16:18:13 +0100

    chore: regen new paigefile

--- .paige/paigefile.py
+++ .paige/paigefile.py
@@ -1,1 +1,14 @@
-import paige
+
+import paige as pg
+
+def main():
+    pg.generate_makefiles(
+        pg.Makefile(
+            path = pg.from_git_root(),
+            default_target = all()
+    ),
+)
+
+pg.task()
+def all():
+    pass
